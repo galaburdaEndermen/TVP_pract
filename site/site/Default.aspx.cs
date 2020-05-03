6e548418701a5325cf4afee5d39414bb32e466a4
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-05-04 01:46:55 +0300

    ахуєть, воно ховери спавнить на ходу, осталось нормально зробить
    от тільки работає сайт тільки для fullHD екранів, пасіба прив*язаності ховерів до пікселів
    як фісить - хз

--- site/site/Default.aspx.cs
+++ site/site/Default.aspx.cs
@@ -10,13 +10,76 @@ namespace site
 {
     public partial class Default : System.Web.UI.Page
     {
+
+        private Control FindControlRecursive(Control rootControl, string controlID)
+        {
+            if (rootControl.ID == controlID) return rootControl;
+
+            foreach (Control controlToSearch in rootControl.Controls)
+            {
+                Control controlToReturn = FindControlRecursive(controlToSearch, controlID);
+                if (controlToReturn != null) return controlToReturn;
+            }
+            return null;
+
+        }
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            HtmlGenericControl sas = new HtmlGenericControl("div");
-            sas.Attributes["class"] = "myCssClass";
+            HtmlGenericControl testCard = new HtmlGenericControl("div"); // сама карточка для теста, чи добавиться вона на сторінку 
+            testCard.Attributes["class"] = "card";
+
+            HtmlGenericControl face1 = new HtmlGenericControl("div");
+            face1.Attributes["class"] = "face face1";
+            HtmlGenericControl face1Content = new HtmlGenericControl("div");
+            face1Content.Attributes["class"] = "content";
+
+            //HtmlGenericControl face1Img = new HtmlGenericControl("img");
+            //face1Img.Attributes["src"] = "pictures\test3.jpg";
+            Image face1Img = new Image();
+            face1Img.ID = "img";
+            face1Img.ImageUrl = @"pictures\test3.jpg";
+ 
+
+            face1Content.Controls.Add(face1Img);
+            face1.Controls.Add(face1Content);
+
+
+
+            HtmlGenericControl face2 = new HtmlGenericControl("div");
+            face2.Attributes["class"] = "face face2";
+            HtmlGenericControl face2Content = new HtmlGenericControl("div");
+            face2Content.Attributes["class"] = "content";
+            HtmlGenericControl face2Text = new HtmlGenericControl("p");
+            face2Text.InnerText = "testing test just for test, you know, test is realy important";
+            HtmlGenericControl face2Link = new HtmlGenericControl("a");
+            face2Link.Attributes["href"] = "#";
+            face2Link.InnerText = "LINK";
+
+            face2Content.Controls.Add(face2Text);
+            face2Content.Controls.Add(face2Link);
+            face2.Controls.Add(face2Content);
+
+
+            testCard.Controls.Add(face1);
+            testCard.Controls.Add(face2);
+
+
+            //var sas = Page.FindControl("kek");
+            
+            var sas = FindControlRecursive(Page, "kek");
+
+
+            HtmlGenericControl maincontainer = (HtmlGenericControl)sas;
+            maincontainer.Controls.Add(testCard);
+        //maincontainer.Controls.Add()
+
+
 
         //https://issue.life/questions/36741781
         //https://progi.pro/aspnet-htmlgenericcontrol-div-obnovit-10253159
+        //https://docs.microsoft.com/en-us/dotnet/api/system.web.ui.page.findcontrol?view=netframework-4.8
         }
     }
 }
--- site/site/Default.aspx.designer.cs
+++ site/site/Default.aspx.designer.cs
@@ -24,12 +24,12 @@ namespace site
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Repeater1 элемент управления.
+        /// kek элемент управления.
         /// </summary>
         /// <remarks>
         /// Автоматически создаваемое поле.
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl kek;
     }
 }
