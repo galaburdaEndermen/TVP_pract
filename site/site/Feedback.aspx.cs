0abde6c572fe9c6ab0ce89e95b71340d01b6c02b
Anton Galaburda <tipa1488amerikos@gmail.com>
2020-05-07 01:22:23 +0300

    ну уже норм пісьма отправляє, і без многопотока

--- site/site/Feedback.aspx.cs
+++ site/site/Feedback.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -16,40 +17,68 @@ namespace site
 
         }
 
+        //private async Task SendEmailAsync()
+        //{
+        //    MailAddress from = new MailAddress("galaburda0test@gmail.com", "Mail from gallery");
+        //    MailAddress to = new MailAddress("tipa1488amerikos@gmail.com");
+        //    MailMessage m = new MailMessage(from, to);
+        //    m.Subject = Subject.Text;
+        //    m.Body = Text.Text + "\n\n\n" + Email.Text + "\t" + Name.Text;
+        //    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+        //    smtp.UseDefaultCredentials = false;
+        //    smtp.Credentials = new NetworkCredential("galaburda0test@gmail.com", "test007A");
+        //    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+        //    smtp.EnableSsl = true;
+        //    await smtp.SendMailAsync(m);
+        //}
+
         protected void Unnamed_Click(object sender, EventArgs e)
         {
-        
+            SendEmailAsync().GetAwaiter();
 
-            try
-            {
-                // отправитель - устанавливаем адрес и отображаемое в письме имя
-                MailAddress from = new MailAddress("galaburda0test@gmail.com", "Mail from gallery");
-                // кому отправляем
-                MailAddress to = new MailAddress("tipa1488amerikos@gmail.com");
-                // создаем объект сообщения
-                MailMessage m = new MailMessage(from, to);
-                // тема письма
-                m.Subject = Subject.Text;
-                // текст письма
-                m.Body = Text.Text + "\n\n\n" + Email.Text + "\t" + Name.Text;
-                // письмо представляет код html
-                //m.IsBodyHtml = true;
-                // адрес smtp-сервера и порт, с которого будем отправлять письмо
-                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-                // логин и пароль
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential("galaburda0test@gmail.com", "test007A");
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtp.EnableSsl = true;
-                smtp.Send(m);
-                Console.Read();
+            //try
+            //{
+            //    // отправитель - устанавливаем адрес и отображаемое в письме имя
+            //    MailAddress from = new MailAddress("galaburda0test@gmail.com", "Mail from gallery");
+            //    // кому отправляем
+            //    MailAddress to = new MailAddress("tipa1488amerikos@gmail.com");
+            //    // создаем объект сообщения
+            //    MailMessage m = new MailMessage(from, to);
+            //    // тема письма
+            //    m.Subject = Subject.Text;
+            //    // текст письма
+            //    m.Body = Text.Text + "\n\n\n" + Email.Text + "\t" + Name.Text;
+            //    // письмо представляет код html
+            //    //m.IsBodyHtml = true;
+            //    // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            //    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            //    // логин и пароль
+            //    smtp.UseDefaultCredentials = false;
+            //    smtp.Credentials = new NetworkCredential("galaburda0test@gmail.com", "test007A");
+            //    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //    smtp.EnableSsl = true;
+            //    smtp.Send(m);
+            //    Console.Read();
 
-            }
-            catch (Exception ek)
-            {
-                string sas = ek.Message;
-                throw;
-            }
+            //}
+            //catch (Exception ek)
+            //{
+            //    string sas = ek.Message;
+            //    throw;
+            //}
+
+
+            MailAddress from = new MailAddress("galaburda0test@gmail.com", "Mail from gallery");
+            MailAddress to = new MailAddress("tipa1488amerikos@gmail.com");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = Subject.Text;
+            m.Body = Text.Text + "\n\n\n" + Email.Text + "\t" + Name.Text;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("galaburda0test@gmail.com", "test007A");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+            smtp.Send(m);
         }
     }
 }
